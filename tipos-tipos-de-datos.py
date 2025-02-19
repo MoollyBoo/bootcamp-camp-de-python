8983acbd0d6e0c5662e16f193d2e1a8f27df5d80
MollyBoo <ara8329@gmail.com>
2025-02-19 10:53:46 -0500

    Se inicia el repositorio Bootcamp de Python

---
+++ tipos-tipos-de-datos.py
@@ -0,0 +1,46 @@
+"string" # string se utiliza para tipos de datos texto
+"string" # Así también podemos escribir un string con comillas dobles
+'string' # y con comillas simples
+"""string""" # Así podemos escribir un string con comillas triples
+"""string""" # Así podemos escribir un string con comillas triples de varias lineas
+'''string''' # Así podemos escribir un string con comillas triples de varias lineas
+
+"""Tus datos son:
+    Nombre: Johanna
+    Apellido: Gallego Castrillón"""
+'''Tus datos son:
+    Nombre: Johanna
+    Apellido: Gallego Castrillón'''
+
+# En python tenemos diferentes tipos de números:
+4 # Enteros o tipos de datos enteros
+4.0 # Flotantes o tipos de datos decimales float
+4j # Complejos o tipos de datos complejos complex 
+
+4+4j # Así podemos combinarlos
+4+4j # Así podemos combinarlos
+
+# En python tenemos diferentes tipos de datos simples (booleanos): 
+True # True o tipo de datos booleano, en Python las escribimos con la primera letra en mayúscula.
+False # False o tipo de datos booleano, en Python las escribimos con la primera letra en mayúscula. 
+
+# En python tenemos diferentes tipos de datos compuestos (listas):
+[1, 2, 3] # Listas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+['string', 2, 3] # Listas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+[1, 'string', 3] # Listas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+
+# En python tenemos diferentes tipos de datos compuestos (tuplas):
+(1, 2, 3) # Tuplas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+('string', 2, 3) # Tuplas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+(1, 'string', 3) # Tuplas o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+
+# En python tenemos diferentes tipos de datos compuestos (conjuntos):
+{1, 2, 3} # Conjuntos o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+{'string', 2, 3} # Conjuntos o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+{1, 'string', 3} # Conjuntos o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+
+# En python tenemos diferentes tipos de datos compuestos (diccionarios):
+{1: 'string', 2: 'string', 3: 'string'} # Diccionarios o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula.
+{'string': 1, 'string': 2, 'string': 3} # Diccionarios o tipos de datos compuestos, en Python las escribimos con la primera letra en mayúscula. 
+
+
